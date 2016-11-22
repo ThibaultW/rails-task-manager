d137c97ca2cef4d53e3496e300cd66c27b2ac9ca
Thibault Willems <thibwillems@scarlet.be>
2016-11-22 15:36:11 +0100

    complete rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
-  # read all tasks
-  get 'tasks', to: 'tasks#index'
-  
-  # read one task
-  get 'tasks/:id', to: 'tasks#show'
-
-  ## create a new task
-  # get the form
-  get 'tasks/new', to: 'tasks#new'
-  # post the form 
-  post 'tasks', to: 'tasks#create'
-  
-  ## update a task
-  # get the form
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  # post the form
-  patch 'tasks/:id', to: 'tasks#update'
-
-  delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks, except: [:destroy]
+#   # read all tasks
+#   get "tasks", to: "tasks#index"
+#   
+#   # read one task
+#   get "tasks/:id", to: "tasks#show", as: :show
+# 
+#   # create a new task
+#   # get the form
+#   get "tasks/new", to: "tasks#new", as: :new
+#   
+#   # post the form 
+#   post "tasks", to: "tasks#create"
+#   
+#   # update a task
+#   # get the form
+#   get "tasks/:id/edit", to: "tasks#edit", as: :edit
+#   # post the form
+#   patch "tasks/:id", to: "tasks#update"
+#   
+  #delete a task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
 end
